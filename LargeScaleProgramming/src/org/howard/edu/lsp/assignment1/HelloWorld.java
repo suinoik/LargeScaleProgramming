ce804fb7e56989cc34daee98f7bceadb7da6fe16
suinoikhioda <suinoikhioda@10.46.130.72>
2022-09-02 16:04:10 -0400

    This is a commit for assignment number 1

---
+++ LargeScaleProgramming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Onwuosiuno Ikhioda
+ * @author suinoikhioda
+ *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
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Large scale!");
+
+	}
+
+}
